ba2e36e22cc2adfb7668b92f86ef1b87dc4662c5
Ejembi_Sunday <s.ejembi@gmail.com>
2022-11-29 21:17:55 -0800

    second commit on python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (i == 101) or (i == 113):
-        continue
-    print("{}".format(chr(i)), end="")
+for a in range(ord('a'), ord('z') + 1):
+        if chr(a) != 'e' and chr(a) != 'q':
+                    print("{:c}".format(a), end='')
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
-    print("{}{}".format((i // 10), (i % 10)), end=", ")
-    i += i
-print("{:d}".format(99))
+for number in range(0, 100):
+    if number == 99:
+        print("{}".format(number))
+    else:
+        print("{:02}".format(number), end=", ")
