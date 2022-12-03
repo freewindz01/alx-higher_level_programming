850aac64abe834720f3abe4e3c6d2a950a1feae5
Ejembi_Sunday <s.ejembi@gmail.com>
2022-12-03 06:59:22 -0800

    all task

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,72 +1,28 @@
-#include "lists.h"
-
+#include <Python.h>
+/*#include <object.h>*/
+/*#include <listobject.h>*/
+/*#include <stdio.h>*/
 /**
- * print_listint - prints all elements of a listint_t list
- * @h: pointer to head of list
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
+  * print_python_list_info - prints python list info
+  * @p: pyobject
+  * Return: void
+  */
+void print_python_list_info(PyObject *p)
 {
-  const listint_t *current;
-  unsigned int n; /* number of nodes */
+	int size, alloc, i;
+	PyObject *obj;
 
-  current = h;
-  n = 0;
-  while (current != NULL)
-    {
-      printf("%i\n", current->n);
-      current = current->next;
-      n++;
-    }
+	size = Py_SIZE(p);
+	alloc = ((PyListObject *)p)->allocated;
 
-  return (n);
-}
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
 
-/**
- * add_nodeint_end - adds a new node at the end of a listint_t list
- * @head: pointer to pointer of first node of listint_t list
- * @n: integer to be included in new node
- * Return: address of the new element or NULL if it fails
- */
-listint_t *add_nodeint_end(listint_t **head, const int n)
-{
-  listint_t *new;
-  listint_t *current;
+	for (i = 0; i < size; i++)
+	{
+		printf("Element %d: ", i);
 
-  current = *head;
-
-  new = malloc(sizeof(listint_t));
-  if (new == NULL)
-    return (NULL);
-
-  new->n = n;
-  new->next = NULL;
-
-  if (*head == NULL)
-    *head = new;
-  else
-    {
-      while (current->next != NULL)
-	current = current->next;
-      current->next = new;
-    }
-
-  return (new);
-}
-
-/**
- * free_listint - frees a listint_t list
- * @head: pointer to list to be freed
- * Return: void
- */
-void free_listint(listint_t *head)
-{
-  listint_t *current;
-
-  while (head != NULL)
-    {
-      current = head;
-      head = head->next;
-      free(current);
-    }
+		obj = PyList_GetItem(p, i);
+		printf("%s\n", Py_TYPE(obj)->tp_name);
+	}
 }
