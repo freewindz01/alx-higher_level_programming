e4c0456f0f1d50253e3e16d17fd1e2dfc752bcc0
Ejembi_Sunday <s.ejembi@gmail.com>
2023-03-13 16:52:00 -0700

    task updated

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-const convertArg = parseInt(process.argv[2]);
-if (isNaN(convertArg)) {
-	  console.log('Not a number');
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
 } else {
-	  console.log('My number: ' + convertArg);
+  console.log('My number:', parseInt(process.argv[2]));
 }
